09539ea3b3e2ad75bd4f74e164dbe084bdfdf45a
Demonik4052 <Demonik4053@gmail.com>
2020-02-17 21:07:51 +0700

    Huffman

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+#include <fstream>
+using std::cin;
+using std::cout;
+using std::endl;
+using std::ifstream;
 
 int main()
 {
+	ifstream fin;
+	fin.open("input_text.txt");
+	if(!fin.is_open())
+	{
+		cout << "Файл не найден. Выход.." << endl;
+		exit(EXIT_FAILURE); 
+	}
+	
+
 	std::cout << "Hello world" << std::endl;
 	return 0;
 }
