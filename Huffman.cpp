21b14543c14208292ff73f7a3a468eb4912bd76a
Demonik4052 <Demonik4053@gmail.com>
2020-02-17 22:09:28 +0700

    Main

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,22 +1,33 @@
 #include <iostream>
 #include <fstream>
 #include "Huffman.h"
+#include "Source.cpp"
 using std::cin;
 using std::cout;
 using std::endl;
 using std::ifstream;
 
-const int SIZE = 256;
+const int SIZE = 128;
 
 int main()
 {
 	ifstream fin;
 	fin.open("input_text.txt");
 	if (!check_open_file(fin)) exit(EXIT_FAILURE);
-	
+
 	int *arr = new int [SIZE];
+	for (int i = 0; i < SIZE; i++) arr[i] = 0;
+	read_text(fin, arr);
+
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << "ASCII код индекса [" << i << "]: "<< arr[i] << endl;
+	}
+	
 
 
+	delete [] arr;
+	fin.close();
 	std::cout << "Hello world" << std::endl;
 	return 0;
 }
