d182dc0b944eb5393351613862617b188b18a388
Demonik4052 <Demonik4053@gmail.com>
2020-02-17 21:18:37 +0700

    Main

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
 #include <fstream>
+#include "Huffman.h"
 using std::cin;
 using std::cout;
 using std::endl;
 using std::ifstream;
 
+const int SIZE = 256;
+
 int main()
 {
 	ifstream fin;
 	fin.open("input_text.txt");
-	if(!fin.is_open())
-	{
-		cout << "Файл не найден. Выход.." << endl;
-		exit(EXIT_FAILURE); 
-	}
+	if (!check_open_file(fin)) exit(EXIT_FAILURE);
 	
+	int *arr = new int [SIZE];
+
 
 	std::cout << "Hello world" << std::endl;
 	return 0;
